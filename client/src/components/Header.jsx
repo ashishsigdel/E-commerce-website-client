c18dd4f8fd8cfc0aa336ac3a452fd9d0c55fa96a
ashish <a.asis.sigdel01@gmail.com>
2024-04-11 20:47:02 +0545

    minor update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
         <div className="flex-col gap-2 sm:mx-auto max-w-6xl pb-3 pt-3 sm:pt-0 mx-2 sm:px-2">
           <div className="sm:flex hidden">
             <Link
-              to={"http://localhost:5174/"}
+              to={"https://e-commerce-website-seller.onrender.com/"}
               className="text-sm text-white mx-2 my-1"
             >
               <span>Become a seller</span>
